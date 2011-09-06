205800b57771db50b165ad7b25844484117eb258
mapnet <mads@mapnet.dk>
2011-09-06 09:58:19 +0200

    second change

--- src/testfile.py
+++ src/testfile.py
@@ -5,8 +5,8 @@ Created on 30/08/2011
 '''
 from reportlab.pdfgen import canvas
 def hello(c):
-    c.drawString(100,120,"Hello Stefan, I am an autogenrated PDF file")
-    c.drawString(100,100,"Will you please show Mads how to use github")
+    c.drawString(100,130,"Hello Poul, I am an autogenrated PDF file")
+    c.drawString(100,100,"lorem ipsum")
 c = canvas.Canvas("hello.pdf")
 hello(c)
 c.showPage()
